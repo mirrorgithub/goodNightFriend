76cdcc0becff2ea61c3e36b58e1c3e5a5b795bbc
Ray Wang <bluefca@gmail.com>
2023-05-30 16:43:16 +0000

    print exception to log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	rescue_from StandardError, :with => :exceptionCatch
+
+	private 
+
+	def exceptionCatch(err)
+		my_logger = Logger.new("#{Rails.root}/log/exception.log")
+		my_logger.info err.backtrace[0..50].join("\n")
+		my_logger.info err.message
+	end
 end
