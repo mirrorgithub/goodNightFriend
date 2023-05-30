e2c90a9eddc6be76f9e224576984d68f9740d42e
Ray Wang <bluefca@gmail.com>
2023-05-30 15:16:36 +0000

    add redirect test

--- spec/requests/user_logins_spec.rb
+++ spec/requests/user_logins_spec.rb
@@ -7,7 +7,7 @@ include ConstHelper
 
 RSpec.describe "UserLogins", type: :request do
 	describe "GET /user_logins" do
-		it "works! (now write some real specs)" do
+		it "check user login" do
 			User.create(name: "Ray")
 
 			post "/api/user/login/", params: {:user_name=>"DummyRay"}
@@ -18,6 +18,11 @@ RSpec.describe "UserLogins", type: :request do
 			post "/api/user/login/", params: {:user_name=>"Ray"}
 			resultJson = JSON.parse(response.body)
 			expect(resultJson["result"]).to eql R_SUCCESS
+		end
+
+		it "check redirect without authentication" do
+			post '/api/set/clock_in/', params: {:user_name=>"DummyName"}
+			expect(response).to have_http_status(302)
 		end
 	end
 end
